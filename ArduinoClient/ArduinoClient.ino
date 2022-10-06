2ed5ec4ed6eb570f29813a650ca343c776b7c700
ChungyiBossi <chungyi.bossi@gmail.com>
2022-10-06 15:41:00 +0800

    [feature] Communicate Raspberry Pi 4B with Arduino MCU

--- ArduinoClient/ArduinoClient.ino
+++ ArduinoClient/ArduinoClient.ino
@@ -2,34 +2,66 @@
 #include <string.h>
 
  
-char bias_data[] = "0,0";
+char bias_data[50];
 const char delim[] = ",";
 char* bias_data_token_ptr;
+float bias_angle[2];
+int rc1_angle = 0;
+int rc2_angle = 0;
+int usb_baud = 9600;
 void setup() {
   Mini.begin(); 
-  Serial.begin(115200);
-  Mini.RC1.set(0);
-  Mini.RC2.set(0);
-  Mini.RC3.set(0);
-  Mini.RC4.set(0);
+  Serial.begin(usb_baud);
+  Mini.RC1.set(rc1_angle);
+  Mini.RC2.set(rc2_angle);
 }
 
 unsigned long messageTimestamp = 0;
 void loop() {
-  uint64_t now = millis();
-  if(now - messageTimestamp > 2000) 
+  // }
+  delay(1000);
+  if (Serial.available() > 0)//判斷串口緩存區有沒有數據
   {
-    if (Serial.available())//判斷串口緩存區有沒有數據
-    {
-      messageTimestamp = now;
-      // String string_data = Serial.readString(); // Read String from Serial port
-      String string_data = "100,-250"; 
-      strcpy(bias_data, string_data.c_str());      // copy string(convert to pointer) to char array. https://blog.csdn.net/xiexu911/article/details/79965330
+    delay(300);
+    String string_data = Serial.readString(); // Read String from Serial port
+    string_data.trim();
+    // https://www.runoob.com/cprogramming/c-function-strcpy.html 
+    // Copy string(convert to pointer) to char array. 
+    // Be careful of destination array too small.
+    strcpy(bias_data, string_data.c_str());      
+    Serial.print("Data:"); Serial.print(bias_data); Serial.print("|Len:"); Serial.print(strlen(bias_data)); Serial.println("|");
+    if (strlen(bias_data) > 0)
+    {    
+      // Get first & second element.
       bias_data_token_ptr = strtok(bias_data, delim);  // tokenize char array, using delim, return a pointer point to first token.
+      int index = 0;
       while (bias_data_token_ptr != NULL) {
-        Serial.println(atof(bias_data_token_ptr));
+        bias_angle[index % 2] = atof(bias_data_token_ptr);
+        Serial.print(index);Serial.print(":");Serial.println(atof(bias_data_token_ptr));
         bias_data_token_ptr = strtok(NULL, delim);
+        index++;
       }
+
+      rc1_angle = (rc1_angle + int(bias_angle[0]) + 360) % 180;
+      rc2_angle = (rc2_angle + int(bias_angle[1]) + 360) % 180;
+      Mini.RC1.set(rc1_angle);
+      Mini.RC2.set(rc2_angle);
+      Mini.RGB1.setRGB(0,255, 0);
+      Mini.RGB2.setRGB(0,0,255);
+      Serial.print("dX:"); Serial.print(bias_angle[0]); Serial.print(", dY:"); Serial.println(bias_angle[1]);
+      delay(1000);
+    } 
+    else
+    {
+      Mini.RGB1.setRGB(255,0,0);
+      Mini.RGB2.setRGB(255,0,0);
+      delay(300);
     }
+
+  } else {
+    Serial.println("No Data In Serial.");
   }
+
+  Serial.flush();
+
 }
---
+++ rpi_usb_random_data.py
@@ -0,0 +1,45 @@
+# -*-coding:UTF-8-*-
+import re
+import time
+import random
+import serial  # 導入serial庫
+# Reference: https://www.twblogs.net/a/5f04ab112f43ae5e2bb6d720?fbclid=IwAR2vjcQhtJKwRMsgHVXhXKf-3J-knV75XqKnrHqp0uwZPnNFFehbhoaHdpE
+# ls /dev/tty*
+port_name = '/dev/tty.usbserial-D307TEN6'  # Mac
+baud_rate = 9600
+# port_name = '/dev/ttyUSB0'  # Pi 4B
+ser = serial.Serial(port_name, baud_rate, timeout=0.3)  # 打開端口，每一秒返回一個消息
+
+x = 0
+y = 0
+try:
+    is_response_get = True
+    # last_time = time.time()
+    while True:
+        now = time.time()
+        dx = round(random.uniform(-180, 180), 2)
+        dy = round(random.uniform(-180, 180), 2)
+        # x = (x + int(dx) + 360) % 180
+        # y = (y + int(dy) + 360) % 180
+        # print(f"x:{x}, y:{y}")
+        # print(f"dx:{dx}, dy:{dy}")
+
+        if is_response_get:
+            print(f"Send Data....**{dx},{dy},**")
+            data_in_string = f"{dx},{dy},".strip()
+            data_to_be_send = data_in_string.encode(encoding='ASCII')
+            print(f"Write....**{data_to_be_send}**")
+            ser.write(data_to_be_send)
+            ser.flush()
+            time.sleep(0.1)
+            last_time = time.time()
+            is_response_get = False
+        response = ser.readall()  # 用response讀取端口的返回值
+        if response:
+            is_response_get = True
+            print("Response:\n", response.decode('UTF8'))  # 進行打印
+        # time.sleep(0.5)
+        print()
+except Exception as e:
+    print("Stop Serial Transmittion, beacause of: ", e)
+    ser.close()  # 拋出異常後關閉端口py
